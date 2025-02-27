8f29800928a53b0f7ecea1a7ccf45d6ce43eef54
Praveen Patidar <praveenpatidar171@gmail.com>
2025-02-27 09:12:16 +0530

    final updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,30 +3,21 @@ dotenv.config();
 
 import express from 'express'
 import cors from 'cors'
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
 import userRoutes from './routes/userRoutes'
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: 'https://exam-atlas-psi.vercel.app',  //use your local url of frontend in development
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
 
 app.use('/api/v1/user', userRoutes);
 
-
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    return app(req, res);
-}
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// })
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
